cd98bdb8ca7bc7c4b699b566db2664186e75c398
Edwin <Edwin@eddesarrollos16>
2015-08-13 07:45:30 -0500

    ultimos

---
+++ ViajesNovaLib/src/pe/com/logistica/bean/negocio/Ruta.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package pe.com.logistica.bean.negocio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.com.logistica.bean.base.BaseNegocio;
+
+/**
+ * @author Edwin
+ *
+ */
+public class Ruta extends BaseNegocio {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7358273367993273258L;
+	
+	private List<Tramo> tramos;
+	private Tramo tramo;
+
+	/**
+	 * @return the tramos
+	 */
+	public List<Tramo> getTramos() {
+		if (tramos == null){
+			tramos = new ArrayList<Tramo>();
+		}
+		return tramos;
+	}
+
+	/**
+	 * @param tramos the tramos to set
+	 */
+	public void setTramos(List<Tramo> tramos) {
+		this.tramos = tramos;
+	}
+
+	/**
+	 * @return the tramo
+	 */
+	public Tramo getTramo() {
+		if (tramo == null){
+			tramo = new Tramo();
+		}
+		return tramo;
+	}
+
+	/**
+	 * @param tramo the tramo to set
+	 */
+	public void setTramo(Tramo tramo) {
+		this.tramo = tramo;
+	}
+
+}
---
+++ ViajesNovaLib/src/pe/com/logistica/bean/negocio/Tramo.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package pe.com.logistica.bean.negocio;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import pe.com.logistica.bean.base.BaseNegocio;
+import pe.com.logistica.bean.base.BaseVO;
+
+/**
+ * @author Edwin
+ *
+ */
+public class Tramo extends BaseNegocio {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6761433969306849962L;
+	
+	
+	private Destino origen;
+	private Date fechaSalida;
+	private Destino destino;
+	private Date fechaLlegada;
+	private BigDecimal precio;
+	
+	
+	/**
+	 * @return the origen
+	 */
+	public Destino getOrigen() {
+		if (origen == null){
+			origen = new Destino();
+		}
+		return origen;
+	}
+	/**
+	 * @param origen the origen to set
+	 */
+	public void setOrigen(Destino origen) {
+		this.origen = origen;
+	}
+	/**
+	 * @return the fechaSalida
+	 */
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+	/**
+	 * @param fechaSalida the fechaSalida to set
+	 */
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+	/**
+	 * @return the destino
+	 */
+	public Destino getDestino() {
+		if (destino == null){
+			destino = new Destino();
+		}
+		return destino;
+	}
+	/**
+	 * @param destino the destino to set
+	 */
+	public void setDestino(Destino destino) {
+		this.destino = destino;
+	}
+	/**
+	 * @return the fechaLlegada
+	 */
+	public Date getFechaLlegada() {
+		return fechaLlegada;
+	}
+	/**
+	 * @param fechaLlegada the fechaLlegada to set
+	 */
+	public void setFechaLlegada(Date fechaLlegada) {
+		this.fechaLlegada = fechaLlegada;
+	}
+	/**
+	 * @return the precio
+	 */
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+	/**
+	 * @param precio the precio to set
+	 */
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+	
+	
+}
